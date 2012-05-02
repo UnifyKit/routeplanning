2c9a44760405f24d4e0a09b1f826beb088c8298d
cyn.shapaita <cyn.shapaita@gmail.com>
2012-05-02 10:27:02 +0000

    checkstyle

--- src/routeplanning/Node.java
+++ src/routeplanning/Node.java
@@ -1,37 +1,52 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package routeplanning;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- *
+ * Class Node.
  * @author CJC
  */
 public class Node {
-    int id;
-    Double latitude;
-    Double longitude;
-    
-    public Node(int id, Double latitude, Double longitude){
-        this.id = id;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-    
-    public Double getLongitude(){
-        return longitude;
-    }
-    
-    public Double getLatitude(){
-        return latitude;
-    }
-    
-    public String asString(){
-        return "("+this.id+"|"+this.latitude+"|"+this.longitude+")";
-                
-    }
+  /**
+   * id Node.
+   */
+  int id;
+  /**
+   * Latitude of the node.
+   */
+  Double latitude;
+  /**
+   * Longitude of the node.
+   */
+  Double longitude;
+  /**
+   * Constructor.
+   * @param id
+   * @param latitude
+   * @param longitude
+   */
+  public Node(int id, Double latitude, Double longitude) {
+    this.id = id;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+  /**
+   * Get longitude of the node.
+   * @return
+   */
+  public Double getLongitude() {
+    return longitude;
+  }
+  /**
+   * Get latitude of the node.
+   * @return
+   */
+  public Double getLatitude() {
+    return latitude;
+  }
+  /**
+  * Get node as String.
+  * @return
+  */
+  public String asString() {
+    return "(" + this.id + "|" + this.latitude + "|" + this.longitude + ")";
+  }
 }
